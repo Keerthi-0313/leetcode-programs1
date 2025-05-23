b3cd3a4c8cf95b1b2bdf967dfec8b983abda9352
Keerthi-0313 <23bit027@kprcas.ac.in>
2025-05-23 14:55:46 +0530

    Time: 7 ms (6.62%) | Memory: 43.8 MB (5.09%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+         String result = "";
+        int count = 0;
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)==' ')
+             count++;
+            if(count == k)
+            break;
+        result += s.charAt(i);
+        }
+        return result;
+    }
+}
