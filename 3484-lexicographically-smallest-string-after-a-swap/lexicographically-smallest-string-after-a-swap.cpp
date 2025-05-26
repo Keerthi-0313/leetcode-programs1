7af5dcaf80a9d83ee17c979472d7bb224e1ea57c
Keerthi-0313 <23bit027@kprcas.ac.in>
2025-05-26 20:25:26 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (66.67%) - LeetSync

---
+++ 3484-lexicographically-smallest-string-after-a-swap/lexicographically-smallest-string-after-a-swap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string getSmallestString(string s) {
+        int n = s.size();
+        for(int i =0;i<n-1;i++)
+       {
+        int d1 = s[i] -'0';
+        int d2 = s[i+1] - '0';
+            if((d1 % 2== d2%2) && d1>d2){
+                    swap(s[i] , s[i+1]);
+                    break;
+                }
+       }
+        return s;
+    }
+};
